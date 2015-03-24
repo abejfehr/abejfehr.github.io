ce1fbb4beef338634c3523d6f3848f4e92dd562b
abejfehr <abe.fehr@gmail.com>
2015-03-24 13:53:12 -0400

    Fix menu and B&W the technology images

--- js/init.js
+++ js/init.js
@@ -20,7 +20,7 @@
 		reset: 'full',
 		containers: '100%',
 		breakpoints: {
-			global: { href: 'http://abefehr.com/css/style.css', grid: { gutters: ['2.5em', 0] } },
+			global: { href: 'css/style.css', grid: { gutters: ['2.5em', 0] } },
 			xlarge: { media: '(max-width: 1800px)', href: 'http://abefehr.com/css/style-xlarge.css' },
 			large: { media: '(max-width: 1280px)', href: 'http://abefehr.com/css/style-large.css', grid: { gutters: ['2em', 0] } },
 			medium: { media: '(max-width: 980px)', href: 'http://abefehr.com/css/style-medium.css'},
